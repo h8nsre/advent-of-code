b49cc037503a997c3dcc43316436329f8ffbd5fe
h8nsre <nguyendangdaihiep@gmail.com>
2020-12-13 16:01:32 +0700

    implement advent of code day 2

--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/h8nsre/advent-of-code
 
 go 1.15
 
+require github.com/thanos-io/thanos v0.14.0
 
-require (
-
-)
+replace k8s.io/client-go => k8s.io/client-go v0.0.0-20190620085101-78d2af792bab
--- utils/file.go
+++ utils/file.go
@@ -1,5 +1,26 @@
-package main
+package utils
 
-func main() {
-	$END$
+import (
+	"bufio"
+	"github.com/thanos-io/thanos/pkg/runutil"
+	"os"
+)
+
+func ScanLine(filename string, f func(string) error) error {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		return err
+	}
+
+	defer runutil.CloseWithErrCapture(&err, file, "close file")
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		if err := f(scanner.Text()); err != nil {
+			return err
+		}
+	}
+	return scanner.Err()
 }
